4e15cf6fe60082f38c97232c961aea4933e5c575
SadmanJawad <jawadnabil977@gmail.com>
2024-07-06 23:16:39 +0600

    adding the background circles with framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
 
 type Props = {};
 
@@ -16,7 +17,8 @@ export default function Hero({}: Props) {
   });
 
   return (
-    <div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
